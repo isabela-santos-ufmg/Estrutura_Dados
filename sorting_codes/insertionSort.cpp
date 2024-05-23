6262617a0251de7709e6bf54e77d0b8c9af9211a
isabela <isabela@debian.isabela-pc>
2024-05-23 20:33:16 -0300

    adicionando insertion sort

---
+++ sorting_codes/insertionSort.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+void printVector(int vector[], int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        std::cout << vector[i] << std::endl;
+    }
+};
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+};
+
+void insertionSort(int vector[], int size)
+{
+    for(int i = 1; i < size; i++)
+    {
+        int aux = vector[i];
+        int j = i - 1;
+        while( j >= 0 && aux < vector[j])
+        {
+            vector[j + 1] = vector[j];
+            j--;
+        }
+        vector[j + 1] = aux;
+    }
+};
+
+int main()
+{
+    int v [] = {5, 2, 1, 0, 4, 3, 6};
+    
+    std::cout << "Original: " << std::endl;
+    printVector(v, std::size(v));
+    insertionSort(v, std::size(v));
+    std::cout << "Ordenado: " << std::endl;
+    printVector(v, std::size(v));
+    return 0;
+}
